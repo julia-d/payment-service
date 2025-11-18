e75070d6e949c4346c62bd36aae1f0ccd14e6abe
julia.denysova <julia.denysova@gmail.com>
2025-11-18 09:36:55 +0200

    Create project skeleton with Intellij Idea Spring Boot constructor

---
+++ src/main/java/org/proxiadsee/test/task/paymentservice/PaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package org.proxiadsee.test.task.paymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentServiceApplication.class, args);
+	}
+
+}
